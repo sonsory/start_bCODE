4899f5ef90a38599feb57d4c4bf5ad341f15cbc6
sonsory <sonsory@naver.com>
2017-11-17 03:33:03 +0900

    fix enviroment and mongodb for heroku

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use("/posts", require("./routes/posts"));
 app.use("/users", require("./routes/users"));
 
 // Port setting
-app.listen(4000, function(){
+var port = process.env.PORT || 4000;
+app.listen(port, function(){
   console.log("server on!")
 })
