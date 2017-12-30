904b847deaa452e2be6577dbfe3c6952e66b017d
sonsory <sonsory@naver.com>
2017-12-31 04:08:44 +0900

    fix the scanImage size11

--- public/js/camera3/main.js
+++ public/js/camera3/main.js
@@ -237,7 +237,7 @@ console.log("video : ", video);
 
 
 // console.log( "js - video : ", video )
-var th = 1 //최초 50, 값을 조절해보니, th값이 작아지면, 스캔된 이미지 사이즈가 좀더 큰 이미지가 되는 듯. 171228
+var th = 0 //최초 50, 값을 조절해보니, th값이 작아지면, 스캔된 이미지 사이즈가 좀더 큰 이미지가 되는 듯. 171228
 // 17.10.06 14:40 아무래도 여기가 비코드 스캔하여 값을 만드는 곳일 듯. 나머지는 내가 수정할 필요가 없고, 여기가 핵심이 될것 같다라는 생각.
 // 기존 며칠간 한 것 처럼, JSON 파일에 저장되던 DB를 모두 MONGODB로 바꾸고, 일부 필요한 것은 추가(예를들어 로그인)하고, 이부분의 스캔 형태를 사각형이 아닌
 //그리고 사각형의 한 변의 크기보다 작은 지름을 갖는 사이즈의 원의 형태를 갖도록 하여 스캔확룰을 보다 높이는(아마도 높아질 것이라 예측) 것이 10월 20일 전후 까지의 목표
@@ -266,7 +266,7 @@ function findPosition(){
 
 
 
-  context.drawImage(video, 292+th, 465+th, 120-th*2, 120-th*2, 0, 0, 120, 120);
+  context.drawImage(video, 291+th, 463+th, 120-th*2, 120-th*2, 0, 0, 100, 100);
 
                       //(소스, 클리핑 시작점x, 클리핑 시작점y, 소스의 x방향길이만큼 가져옴, 소스의 y방향길이 방향만큼 가져옴, 가져온 이미지의 새로운 x시작점 지정, 가져온 이미지의 새로운 y 시작점 지정, x방향 길이(배율조절됨), y 방향 길이(배율 조절됨) ... 배율조절 된다는게.. 내가쓴 글인데.. 뭔말..
                        //original 160+th, 240+th, 160-th*2, 160-th*2, 0, 0, 150, 150);
@@ -280,7 +280,7 @@ function findPosition(){
                       // y	      The y coordinate where to place the image on the canvas	Play it »
                       // width	  Optional. The width of the image to use (stretch or reduce the image)	Play it »
                       // height	  Optional. The height of the image to use (stretch or reduce the image)
-  var imageData = context.getImageData(0, 0, 120, 120); // -original: context.getImageData(0, 0, 150, 150);
+  var imageData = context.getImageData(0, 0, 100, 100); // -original: context.getImageData(0, 0, 150, 150);
   console.log("public/js/main.js imageData = ", imageData);
   var data = imageData.data;
     console.log("public/js/main.js imageData.data = ", imageData.data);
