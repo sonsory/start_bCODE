7d1188e2b9c19c7b7aa0ab409d7d9d3ac04f2bbc
sonsory <sonsory@naver.com>
2018-01-31 00:46:36 +0900

    Modify the position of CaptureBox09

--- public/js/camera3/main.js
+++ public/js/camera3/main.js
@@ -171,7 +171,8 @@ function init(){
                 console.log( "captureDivElement: ", captureDivElement )
                 console.log( "captureDivElement.domElement : ", video )
                 video.setAttribute("id", "video"); //html 태그의 속성 추가, 수정하는 방법
-                video.setAttribute("style", "margin:0 auto; width:200px; height:270px; position: relative; z-index: -3;border: 1px solid gold"); //width:141px; height:188px;"); //margin:0 auto; width:200px; height:270px; position: relative; z-index: -3;border: 1px solid gold  //margin:0 auto; width:200px; position: relative; z-index: -3  원본! 180130
+                video.setAttribute("style", "margin:0 auto; width:auto; height:auto; max-width:100%; position: relative; z-index: -3;border: 1px solid gold"); //width:141px; height:188px;"); //margin:0 auto; width:200px; height:270px; position: relative; z-index: -3;border: 1px solid gold  //margin:0 auto; width:200px; position: relative; z-index: -3  원본! 180130
+    //width:200px; height:270px;
 
 
                 console.log( "video id : ", video.getAttribute("id") )
@@ -413,7 +414,7 @@ function wholeCapture(){
 
   var video = document.getElementById('video');
   //context.scale(0.1, 0.1);
-  context.drawImage(video, 0, 0, canvas.width, canvas.height); /**/     
+  context.drawImage(video, 0, 0, canvas.width, canvas.height); /**/
    // source rectangle  canvas.width, canvas.height
   var img    = canvas.toDataURL("image/png");
 
